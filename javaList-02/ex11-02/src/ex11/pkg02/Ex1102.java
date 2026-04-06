a8d360ebb8aa33070ef775f4d3cf93ebaf2b54ff
Márcio Simão <marciosimao0825@gmail.com>
2026-04-06 11:25:22 +0100

    .

--- javaList-02/ex11-02/src/ex11/pkg02/Ex1102.java
+++ javaList-02/ex11-02/src/ex11/pkg02/Ex1102.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ex11.pkg02;
-
+import java.util.Scanner;
 /**
  *
  * @author LENOVO
@@ -15,6 +15,7 @@ public class Ex1102 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
     }
     
 }
