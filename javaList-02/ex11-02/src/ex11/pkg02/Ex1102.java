cd6a510fee480a857454497a848dbb738da75782
Márcio Simão <marciosimao0825@gmail.com>
2026-04-06 11:56:53 +0100

    Update Ex1102.java

--- javaList-02/ex11-02/src/ex11/pkg02/Ex1102.java
+++ javaList-02/ex11-02/src/ex11/pkg02/Ex1102.java
@@ -16,7 +16,44 @@ public class Ex1102 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        double salárioActual;
+        double salarioActual, salarioNovo;
+        String genero;
+        int  anosTrabalho;
+        
+        System.out.print("Salário actual: ");
+        salarioActual = input.nextDouble();
+        
+        System.out.print("Anos de trabalho: ");
+        anosTrabalho = input.nextInt();
+        
+        System.out.print("Género: ");
+        genero = input.nextLine();
+        
+        switch (genero.toLowerCase())
+        {
+            case "homem" :
+                if(anosTrabalho < 20)
+                {
+                   salarioNovo =   salarioActual + (salarioActual * 0.03);
+                }else{
+                    if((anosTrabalho >= 20) && (anosTrabalho <= 30))
+                    {
+                        salarioNovo =   salarioActual + (salarioActual * 0.13);
+                    }else{
+                        if(anosTrabalho > 30)
+                        {
+                            salarioNovo =   salarioActual + (salarioActual * 0.25);
+                        }    
+                    }    
+                }    
+            break;     
+            case "mulher" :
+                
+            break;
+            default :
+                System.out.println("Não existe esse genero seu/sua panina!!");
+            break;    
+        }    
     }
     
 }
