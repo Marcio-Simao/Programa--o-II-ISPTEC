b2d5bb821b2dd98ea39233a5db70a24b75c120d8
Márcio Simão <marciosimao0825@gmail.com>
2026-04-06 11:23:35 +0100

    novo projeto ex11-02

---
+++ javaList-02/ex11-02/src/ex11/pkg02/Ex1102.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex11.pkg02;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Ex1102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
