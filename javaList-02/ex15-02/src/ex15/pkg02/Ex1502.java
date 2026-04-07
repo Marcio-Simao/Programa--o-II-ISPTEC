f405181e6ebff76475c4090674fd256845fbbdb3
Márcio Simão <marciosimao0825@gmail.com>
2026-04-07 18:52:42 +0100

    .

--- javaList-02/ex15-02/src/ex15/pkg02/Ex1502.java
+++ javaList-02/ex15-02/src/ex15/pkg02/Ex1502.java
@@ -1,9 +1,20 @@
-package ex13.pkg02;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex15.pkg02;
+
 import java.util.Scanner;
 
-public class Ex1302 {
+/**
+ *
+ * @author LENOVO
+ */
+public class Ex1502 {
 
-   
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
