bafa4befac0e5214ca9c025bf26129c9ceb59ee1
Márcio Simão <marciosimao0825@gmail.com>
2026-04-27 11:00:39 +0100

    Update Ex1803.java

--- javaList-03/ex1803/src/ex1803/Ex1803.java
+++ javaList-03/ex1803/src/ex1803/Ex1803.java
@@ -16,7 +16,8 @@ public class Ex1803 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        int i = 0;
+        int i = 0, produto = 0;
+        double totVendas = 0;
         do
         {
             System.out.println("Cantina do Mama Du");
@@ -26,7 +27,28 @@ public class Ex1803 {
             switch(i)
             {
                 case 1:
-                    System.out.println("Nova compra!");
+                    System.out.println("P r o d u t o s");
+                    System.out.println("[1] - Café - 100 kz");
+                    System.out.println("[2] - Capuccino – 250 kz ");
+                    System.out.println("[3] - Galão - 200 kz ");
+                    System.out.println("Total de Vendas: "+ totVendas);
+                    produto = input.nextInt();
+                    
+                    switch(produto)
+                    {
+                        case 1:
+                            
+                        break;  
+                        case 2:
+                            
+                        break;
+                        case 3:
+                            
+                        break;
+                        default :
+                            
+                        break;
+                    }
                  break;
                 case 0: 
                     System.out.println("Loja fechada");
