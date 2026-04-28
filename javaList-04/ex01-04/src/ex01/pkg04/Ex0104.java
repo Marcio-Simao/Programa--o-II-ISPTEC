823a1ac57dc169762741e0d421f6eee451821300
Márcio Simão <marciosimao0825@gmail.com>
2026-04-28 16:12:12 +0100

    .

---
+++ javaList-04/ex01-04/src/ex01/pkg04/Ex0104.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex01.pkg04;
+import java.util.Scanner;
+/**
+ *
+ * @author LENOVO
+ */
+public class Ex0104 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int [] num = new int[5];
+        
+        for(int i = 0; i > 5; i++)
+        {
+            System.out.print(i+" num: ");
+            num[i] = input.nextInt();
+        }
+    }
+    
+}
