4e6308343cd585a949289b31d804d78207834ab1
Márcio Simão <marciosimao0825@gmail.com>
2026-04-28 15:49:21 +0100

    Update Ex1903.java

--- javaList-03/ex19-03/src/ex19/pkg03/Ex1903.java
+++ javaList-03/ex19-03/src/ex19/pkg03/Ex1903.java
@@ -18,9 +18,9 @@ public class Ex1903 {
         Scanner input = new Scanner(System.in);
         int quant = 1;
         
-        for(int i = 1; i <= 3; i++)
+        for(int i = 1; i <= 5; i++)
         {
-            for(int j = 1; j <= 3; j++)
+            for(int j = 1; j <= 5; j++)
             {
                 if(i == 1 && j == 1)
                 {
