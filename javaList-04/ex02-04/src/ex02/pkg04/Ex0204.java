ede61e212f5aafe1cddc3aa9ef1dee40a639ed30
Márcio Simão <marciosimao0825@gmail.com>
2026-05-05 17:14:27 +0100

    .

---
+++ javaList-04/ex02-04/src/ex02/pkg04/Ex0204.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex02.pkg04;
+import java.util.Scanner;
+/**
+ *
+ * @author LENOVO
+ */
+public class Ex0204 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int [][] matriz = new int [5][5];
+        
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                System.out.println("[ "+ i +" ][ "+ j +" ]");
+            }
+        }
+    }
+    
+}
