207441f9c024ed674a9af5c06cb661c6c22aee21
Márcio Simão <marciosimao0825@gmail.com>
2026-05-06 09:50:50 +0100

    .

--- javaList-04/ex05-04/src/ex05/pkg04/Ex0504.java
+++ javaList-04/ex05-04/src/ex05/pkg04/Ex0504.java
@@ -2,27 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ex05.pkg05;
-import java.util.Scanner;
-import java.util.ArrayList;
+package ex05.pkg04;
+
 /**
  *
  * @author LENOVO
  */
-public class Ex0505 {
+public class Ex0504 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner input = new Scanner (System.in);
-        ArrayList<Integer> listNum = new ArrayList<Integer>();
-        ArrayList<String> listaNomes = new ArrayList<String>(); 
-        
-        listaNomes.add("Márcio");
-        
-        System.out.println(listaNomes.get(0));
     }
     
 }
