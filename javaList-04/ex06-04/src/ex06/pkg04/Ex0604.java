da4783a2cd067947cc1e53956727d2322327507d
Márcio Simão <marciosimao0825@gmail.com>
2026-05-08 12:44:02 +0100

    .

--- javaList-04/ex06-04/src/ex06/pkg04/Ex0604.java
+++ javaList-04/ex06-04/src/ex06/pkg04/Ex0604.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ex06.pkg04;
-
+import java.util.Scanner;
 /**
  *
  * @author LENOVO
@@ -15,6 +15,23 @@ public class Ex0604 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[][] matriz = new int [7][8];
+        
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                matriz[i][j] = i + j; 
+            }
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                System.out.print(matriz[i][j]+" "); 
+            }
+            System.out.println(" ");
+        }
     }
     
 }
