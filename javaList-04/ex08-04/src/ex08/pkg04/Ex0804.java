fe506a459245a72d3dff9f7b3a5ed090d859e5bc
Márcio Simão <marciosimao0825@gmail.com>
2026-05-08 23:32:47 +0100

    .

--- javaList-04/ex08-04/src/ex08/pkg04/Ex0804.java
+++ javaList-04/ex08-04/src/ex08/pkg04/Ex0804.java
@@ -34,6 +34,14 @@ public class Ex0804 {
                 }
             }
         }
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println(" ");
+        }    
     }
     
 }
