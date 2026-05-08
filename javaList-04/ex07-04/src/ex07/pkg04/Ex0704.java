98d2a987bc50552d0589b4f9ba6b445ad4170b3d
Márcio Simão <marciosimao0825@gmail.com>
2026-05-08 12:57:47 +0100

    .

---
+++ javaList-04/ex07-04/src/ex07/pkg04/Ex0704.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex07.pkg04;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author LENOVO
+ */
+public class Ex0704 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int[][] equipas = new int [10][5];
+        int[] maiorAltura = new int [5];
+        
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 5; j++) 
+            {
+                
+            }
+        }
+    }
+    
+}
