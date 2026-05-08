87b241f0f74e671947a20985b139b3f6f1acf00b
Márcio Simão <marciosimao0825@gmail.com>
2026-05-08 22:08:10 +0100

    .

--- javaList-04/ex07-04/src/ex07/pkg04/Ex0704.java
+++ javaList-04/ex07-04/src/ex07/pkg04/Ex0704.java
@@ -18,15 +18,27 @@ public class Ex0704 {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-        int[][] equipas = new int [10][5];
+        double[][] equipas = new double [5][3];
         int[] maiorAltura = new int [5];
         
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < 5; i++)
         {
-            for(int j = 0; j < 5; j++) 
+            System.out.println(i+"Equipa");
+            for(int j = 0; j < 3; j++) 
             {
-                
+                System.out.println();
+                equipas[i][j] = input.nextDouble();
             }
+            System.out.println("-----------");
+        }
+        
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 3; j++) 
+            {
+                System.out.print(equipas[i][j]+" "); 
+            }
+            System.out.println(" ");
         }
     }
     
