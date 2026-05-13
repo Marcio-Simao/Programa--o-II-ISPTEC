b2e56df35ff51e65835c481bea42e371fb36ed17
Márcio Simão <marciosimao0825@gmail.com>
2026-05-13 17:44:50 +0100

    Update Ex0206.java

--- javaList-06/ex02-06/src/ex02/pkg06/Ex0206.java
+++ javaList-06/ex02-06/src/ex02/pkg06/Ex0206.java
@@ -16,7 +16,7 @@ public class Ex0206 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        
+        int numCar1, numCar2;
         String pala1, pala2;
         
         System.out.println("Digite a primeira palavra: ");
@@ -25,9 +25,12 @@ public class Ex0206 {
         System.out.println("Digite a segunda palavra: ");
         pala2 = input.nextLine();
         
+        numCar1 = pala1.length();
+        numCar2 = pala2.length();
+        
         //string1.regionMatches(inicio1, string2, inicio2, quantidade)
         //regionMatches(true, inicio1, string2, inicio2, quantidade)
-        if(pala1.regionMatches(true,0,pala2,0,5))
+        if(pala1.regionMatches(true,0,pala2,0,numCar1))
         {
             System.out.println("São iguais");
         }else{
