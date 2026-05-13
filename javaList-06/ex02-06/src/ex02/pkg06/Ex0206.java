e76e22954cddb1235ca8c985899602d225d106d2
Márcio Simão <marciosimao0825@gmail.com>
2026-05-13 17:22:50 +0100

    .

---
+++ javaList-06/ex02-06/src/ex02/pkg06/Ex0206.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex02.pkg06;
+import java.util.Scanner;
+/**
+ *
+ * @author LENOVO
+ */
+public class Ex0206 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        String pala1, pala2;
+        
+        System.out.println("Digite a primeira palavra: ");
+        pala1 = input.nextLine();
+        
+        System.out.println("Digite a segunda palavra: ");
+        pala2 = input.nextLine();
+        
+        //string1.regionMatches(inicio1, string2, inicio2, quantidade)
+        //regionMatches(true, inicio1, string2, inicio2, quantidade)
+        if(pala1.regionMatches(true,0,pala2,0,5))
+        {
+            System.out.println("São iguais");
+        }else{
+            System.out.println("Não são iguais");
+        }
+    }
+    
+}
