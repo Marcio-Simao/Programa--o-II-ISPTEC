3babe68b27cd1c5f3175dbcdd077e2186b3c940c
Márcio Simão <marciosimao0825@gmail.com>
2026-05-17 21:30:01 +0100

    .

---
+++ javaList-07/src/javalist/pkg07/JavaList07.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javalist.pkg07;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaList07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int ex;
+        System.out.print("Qual exercício ques testar(1 à 16): ");
+        ex = input.nextInt();
+        
+        if(ex == 1){
+            
+        }
+    }
+    
+}
