33054d9a95ec854822d9d1f5156693033cb490c7
Márcio Simão <marciosimao0825@gmail.com>
2026-05-19 11:57:48 +0100

    ex02

--- javaList-07/isptec.pii.Lista7/src/isptec/pii/lista7/Ex02.java
+++ javaList-07/isptec.pii.Lista7/src/isptec/pii/lista7/Ex02.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package isptec.pii.lista7;
+import java.util.Scanner;
 
 /**
  *
  * @author LENOVO
  */
 public class Ex02 {
+    public static void relatorio(String nom){
+        System.out.println("========================================"); 
+        System.out.println("ISPTEC – Instituto Superior Politécnico de Tecnologias e Ciências"); 
+        System.out.println("Disciplina de Programação II");  
+        System.out.println("Nome: "+ nom);               
+        System.out.println("========================================");
+        
+        
+    }
     
 }
--- javaList-07/isptec.pii.Lista7/src/isptec/pii/lista7/IsptecPiiLista7.java
+++ javaList-07/isptec.pii.Lista7/src/isptec/pii/lista7/IsptecPiiLista7.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package isptec.pii.lista7;
-
+import java.util.Scanner;
 /**
  *
  * @author LENOVO
@@ -15,7 +15,26 @@ public class IsptecPiiLista7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Ex1.taboada();
+        Scanner input = new Scanner(System.in); 
+        int num;
+        System.out.print("Digite o número do exercicio: ");
+        num = input.nextInt();
+        if(num == 0 || num < 0){
+            System.out.println("Valor invalido!");
+        }else{
+            if(num == 1){
+                Ex1.taboada();
+            }else{
+                if(num == 2){
+                    String nome;
+                    input.nextLine();
+                    System.out.print("Nome do Aluno: ");
+                    nome = input.nextLine();
+                    Ex02.relatorio(nome);
+                }
+            }
+        }
+        
     }
     
 }
