a2c9181e39b130213fe2372bdc0cc324f3bd2529
Márcio Simão <marciosimao0825@gmail.com>
2026-05-27 12:20:40 +0100

    Update Ex0904.java

--- javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
+++ javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ex09.pkg04;
-
+import java.util.Random;
 /**
  *
  * @author LENOVO
@@ -16,10 +16,18 @@ public class Ex0904 {
     public static void main(String[] args) {
        // TODO code application logic here
         int[][] alunos = new int[10][3];
+        Random rand = new Random();
+        int aluno, prova;
         
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 3; j++){
-                
+                alunos[i][j] = rand.nextInt(21);
+            }
+        }
+        System.out.println("1ª PROVA | 2ª PROVA | 3ª PROVA");
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(alunos[i][j] + "\t");
             }
         }
         
