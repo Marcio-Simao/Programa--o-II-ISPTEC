921d551ed45c87a8f141352b3aaecc33a89fec25
Márcio Simão <marciosimao0825@gmail.com>
2026-05-27 13:11:38 +0100

    Update Ex0904.java

--- javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
+++ javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
@@ -17,11 +17,19 @@ public class Ex0904 {
        // TODO code application logic here
         int[][] alunos = new int[10][3];
         Random rand = new Random();
-        int aluno, prova;
-        
+        int piorProva1=20, piorProva2, piorProva3;
+        int piorAluno1 = 0;
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 3; j++){
                 alunos[i][j] = rand.nextInt(21);
+            }
+        }
+        for(int j = 0; j < 3; j++){
+            for(int i = 0; i < 10; i++){
+                if(piorProva1 < alunos[i][j]){
+                    piorAluno1 = j;
+                }
+                
             }
         }
         System.out.println("1ª PROVA | 2ª PROVA | 3ª PROVA");
@@ -31,7 +39,7 @@ public class Ex0904 {
             }
             System.out.println(" ");
         }
-        
+        System.out.println(" Aluno "+ piorAluno1);
     }
     
 }
