f3c4e1730887c276fd2ab57cac3ee1cb9b5a30b3
Márcio Simão <marciosimao0825@gmail.com>
2026-05-27 12:00:46 +0100

    .

--- javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
+++ javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
@@ -2,18 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ex08.pkg04;
+package ex09.pkg04;
+
 /**
  *
  * @author LENOVO
  */
-public class Ex0804 {
+public class Ex0904 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       // TODO code application logic here
         int[][] matriz = new int[10][10];
         for(int i = 0; i < 10; i++)
         {
@@ -25,9 +26,9 @@ public class Ex0804 {
                 }else{
                     if(i > j)
                     {
-                        matriz[i][j] = (int)(4 * Math.pow(i, 3)) - (int)(5 * Math.pow(j, 2)) + 1;
+                        matriz[i][j] = (4 * i * 3) - (5 * j * 2) + 1;
                     }else{
-                        matriz[i][j] = (int)(3 * Math.pow(i, 2)) -1;
+                        matriz[i][j] = (3 * i * 2) -1;
                     }
                 }
             }
@@ -37,9 +38,10 @@ public class Ex0804 {
         {
             for(int j = 0; j < 10; j++)
             {
-                System.out.print(matriz[i][j] + "\t");
+                System.out.print(matriz[i][j]);
             }
             System.out.println(" ");
-        }    
-    } 
+        } 
+    }
+    
 }
