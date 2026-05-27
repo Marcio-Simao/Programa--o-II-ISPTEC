c603ff8c9ce0266515de7132d7dc48a76de46008
Márcio Simão <marciosimao0825@gmail.com>
2026-05-27 12:04:57 +0100

    Update Ex0904.java

--- javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
+++ javaList-07/ex09-04/src/ex09/pkg04/Ex0904.java
@@ -15,33 +15,10 @@ public class Ex0904 {
      */
     public static void main(String[] args) {
        // TODO code application logic here
-        int[][] matriz = new int[10][10];
-        for(int i = 0; i < 10; i++)
-        {
-            for(int j = 0; j < 10; j++)
-            {
-                if(i < j)
-                {
-                    matriz[i][j] = (2 * i) + (7 * j) - 2;
-                }else{
-                    if(i > j)
-                    {
-                        matriz[i][j] = (4 * i * 3) - (5 * j * 2) + 1;
-                    }else{
-                        matriz[i][j] = (3 * i * 2) -1;
-                    }
-                }
-            }
-        }
+        int[][] alunos = new int[10][3];
         
-        for(int i = 0; i < 10; i++)
-        {
-            for(int j = 0; j < 10; j++)
-            {
-                System.out.print(matriz[i][j]);
-            }
-            System.out.println(" ");
-        } 
+        
+        
     }
     
 }
