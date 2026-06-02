6528eb22dbf06bc369ad751241c83a0c6bc11985
Márcio Simão <marciosimao0825@gmail.com>
2026-06-02 13:39:05 +0100

    .

---
+++ javaList-04/ex14-04/src/ex14/pkg04/Ex1404.java
@@ -0,0 +1,12 @@
+package ex14.pkg04;
+
+import java.util.Scanner;
+
+public class Ex1404 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        arrayList<intenger> lista = new arrayList<>();
+        
+        
+    }
+}
