04f0306c5b5b1c8602898e7148a7250eb34b1f1a
Márcio Simão <marciosimao0825@gmail.com>
2026-06-02 10:04:14 +0100

    Update Ex02Aula.java

--- javaList-04/ex02-Aula/src/ex02/aula/Ex02Aula.java
+++ javaList-04/ex02-Aula/src/ex02/aula/Ex02Aula.java
@@ -2,8 +2,10 @@ package ex02.aula;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class Ex02Aula {
+
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
@@ -16,11 +18,11 @@ public class Ex02Aula {
         for(int i = 0; i < 10; i++){
            num[i+ 10] = num[9 - i];
         }
-        
-           System.out.printf("%.2f", Arrays.toString(num));
+        for(int i = 0; i < 20; i++){
+           System.out.printf("%.2f", num[i]);
            System.out.println(" ");
            
-        
+        }
         
     }
     
