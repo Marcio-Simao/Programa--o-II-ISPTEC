82c26c4a7b4d2b2eda51fc6933fc2d368c5dc53e
Márcio Simão <marciosimao0825@gmail.com>
2026-06-02 14:18:17 +0100

    Update Ex1404.java

--- javaList-04/ex14-04/src/ex14/pkg04/Ex1404.java
+++ javaList-04/ex14-04/src/ex14/pkg04/Ex1404.java
@@ -17,16 +17,19 @@ public class Ex1404 {
         int l2 = input.nextInt();
         System.out.print("Quantos números quês colocar na lista 3: ");
         int l3 = input.nextInt();
+        System.out.println("Lista 1");
         for(int i = 0; i < l1; i++){
             System.out.print("Insira o número: ");
             int n = input.nextInt();
             lista1.add(n);
         }
+        System.out.println("Lista 2");
         for(int i = 0; i < l2; i++){
             System.out.print("Insira o número: ");
             int n = input.nextInt();
             lista2.add(n);
         }
+        System.out.println("Lista 3");
         for(int i = 0; i < l3; i++){
             System.out.print("Insira o número: ");
             int n = input.nextInt();
@@ -35,6 +38,15 @@ public class Ex1404 {
         System.out.println(lista1);
         System.out.println(lista2);
         System.out.println(lista3);
-            
+        
+        grupos.add(lista1);
+        grupos.add(lista2);
+        grupos.add(lista3);
+        System.out.println(grupos);  
+        
+        System.out.print("Digite um número: ");
+        int num = input.nextInt();
+        
+        
     }
 }
