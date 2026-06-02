a41b7c46c7a9f27738c336b58986ce26a7c215a5
Márcio Simão <marciosimao0825@gmail.com>
2026-06-02 10:48:22 +0100

    Update Ex1304.java

--- javaList-04/ex13-04/src/ex13/pkg04/Ex1304.java
+++ javaList-04/ex13-04/src/ex13/pkg04/Ex1304.java
@@ -20,44 +20,72 @@ public class Ex1304 {
         char[] vetor3 = new char[2];
         char[] vetor4 = new char[2];
         char[] vetor5 = new char[2];
-        
+        //Adicionando as letras no array 1 
         System.out.println("Array 1");
         for(int i = 0; i < 2; i++){
             System.out.print("Digite a "+ ( i + 1 ) +"º letra: ");
             vetor1[i] = input.next().charAt(0); 
         }
-        
+        //Adicionando as letras no array 2
         System.out.println("Array 2");
         for(int i = 0; i < 2; i++){
             
            System.out.print("Digite a "+ ( i + 1 ) +"º letra: ");
             vetor2[i] = input.next().charAt(0);
         }
+        //Adicionando as letras no array 3
         System.out.println("Array 3");
         for(int i = 0; i < 2; i++){
             
             System.out.print("Digite a "+ ( i + 1 ) +"º letra: ");
             vetor3[i] = input.next().charAt(0);
         }
+        //Adicionando as letras no array 4
         System.out.println("Array 4");
         for(int i = 0; i < 2; i++){
             System.out.print("Digite a "+( i + 1 )+"º letra: ");
             vetor4[i] = input.next().charAt(0);
         }
+        //Adicionando as letras no array 5
         System.out.println("Array 5");
         for(int i = 0; i < 2; i++){
             System.out.print("Digite a "+ ( i + 1 ) +"º letra: ");
             vetor5[i] = input.next().charAt(0);
         }
+        //Adicionando os vetores no arrayList
         lista.add(vetor1);
         lista.add(vetor2);
         lista.add(vetor3);
         lista.add(vetor4);
         lista.add(vetor5);
-        System.out.print(lista);
+        //Apresentar o arrayList
+        for(int i = 0; i < lista.size(); i++){
+            char[] vetor = lista.get(i);
+            for(int j = 0; j < vetor.length; j++){
+                System.out.print(vetor[j] + " ");
+            }
+            System.out.println(" ");
+        }
+        //Pedindo a outra letra
         System.out.print("Digite uma letra: ");
         letra = input.next().charAt(0);
-        
+        //removendo o vetor
+        for(int i = 0; i < lista.size(); i++){
+            char[] vetor = lista.get(i);
+            
+            if(vetor[0] == letra || vetor[1] == letra){
+                lista.remove(i);
+                i--;
+            }
+        }
+        //Apresentar o arrayList
+        for(int i = 0; i < lista.size(); i++){
+            char[] vetor = lista.get(i);
+            for(int j = 0; j < vetor.length; j++){
+                System.out.print(vetor[j] + " ");
+            }
+            System.out.println(" ");
+        }
     }
     
 }
