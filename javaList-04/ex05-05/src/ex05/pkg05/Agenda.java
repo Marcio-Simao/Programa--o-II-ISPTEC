fbd466f8e959e90e87145505895dbbec7c04c962
Márcio Simão <marciosimao0825@gmail.com>
2026-06-03 10:39:05 +0100

    .

--- javaList-04/ex05-05/src/ex05/pkg05/Agenda.java
+++ javaList-04/ex05-05/src/ex05/pkg05/Agenda.java
@@ -11,6 +11,13 @@ public class Agenda {
         this.numero = numero;
     }
     // Getters — lêem os valores dos campos
-    
-    
+    public String getNome(){return nome;}
+    public int getNumero(){return numero;}
+    // Setters — alteram os valores dos campos
+    public void setNome(String nome){this.nome = nome;}
+    public void setNumero(int numero){this.numero = numero;}
+    // mostra número
+    public void mostrarnumero(){
+        System.out.println("Número: "+ numero);
+    }
 }
--- javaList-05/ex05-05/src/ex05/pkg05/Ex0505.java
+++ javaList-05/ex05-05/src/ex05/pkg05/Ex0505.java
@@ -1,7 +1,10 @@
 package ex05.pkg05;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Ex0505 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        ArrayList<Agenda> agenda = new ArrayList<>();
     }
     
 }
