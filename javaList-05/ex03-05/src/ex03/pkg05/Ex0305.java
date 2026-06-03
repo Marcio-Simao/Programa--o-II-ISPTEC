426fb2799a71d06eeb31d88fe07339bcc700da6a
Márcio Simão <marciosimao0825@gmail.com>
2026-06-03 03:28:50 +0100

    Update Ex0305.java

--- javaList-05/ex03-05/src/ex03/pkg05/Ex0305.java
+++ javaList-05/ex03-05/src/ex03/pkg05/Ex0305.java
@@ -4,8 +4,8 @@ public class Ex0305 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Aluno[] alunos = new Aluno[4];
-        
         for(int i = 0; i < 4; i++){
+            alunos[i] = new Aluno();
             System.out.println((i+1)+"º Aluno ");
             System.out.print("Nome do Aluno: ");
             alunos[i].setNomeAluno(input.nextLine());
@@ -13,6 +13,7 @@ public class Ex0305 {
             alunos[i].setNumAluno(input.nextInt());
             System.out.print("Média do Aluno: ");
             alunos[i].setMediaAluno(input.nextInt());
+            input.nextLine();
         }
         Aluno melhor = alunos[0];
         for(int i = 0; i < 4; i++){ 
@@ -21,6 +22,5 @@ public class Ex0305 {
             }
         }
         melhor.MelhorAluno();
-    }
-    
+    } 
 }
